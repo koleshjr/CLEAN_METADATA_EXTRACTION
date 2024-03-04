96154de2eaf535b0e89f70d5270d6a4be24bf016
koleshjr <stevekolesh975@gmail.com>
2024-03-04 13:25:22 +0300

    corrrects bugs with the submission

--- src/models/extractor.py
+++ src/models/extractor.py
@@ -5,7 +5,7 @@ class MetadataOutput(BaseModel):
     """fields to be extracted from the gazette"""
     gazette_notice_number: str = Field(..., description="extract the gazette notice number from the information.")
     land_holder_names: str = Field(..., description="extract comma separated values of the land holder name or names from the information.")
-    land_registration_numbers: str = Field(..., description="extract the land registration number or numbers from the information.")
+    land_registration_numbers: str = Field(..., description="extract the land registration number or numbers from the information. Don't include the identifiers e.g LR No , Plot No just the values")
     land_location: str = Field(..., description="extract the full land location as mentioned in the information ")
 
 class ExtractionOutput(BaseModel):
