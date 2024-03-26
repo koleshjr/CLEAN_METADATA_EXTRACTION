1b4c14ecfe62f4d8f809bf075140ebf97ea9ea7b
koleshjr <stevekolesh975@gmail.com>
2024-03-26 14:33:12 +0300

    updated codebase

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,6 +1,32 @@
 class Config:
     extraction_prompt  = """A page text will be passed to you. Extract from it all gazette metadata present in the page. The metadata to be extracted includes the gazette notice number, land holder names, land registration numbers, and land location.
     Do not make up ANY extra information. Only extract the information that is present in the page text. If the information is not present, leave the field empty.
+    Example enclosed in back:
+                [{{
+                    "gazette_notice_number": 58,
+                    "land_holder_names": "Lawrence John Irungu Mwangi",
+                    "land_registration_numbers": "Thika Municipality/Block 20/1527",
+                    "land_location": "district of Thika"
+                }},
+                {{
+                    "gazette_notice_number": 996,
+                    "land_holder_names": "Jackson Munyi",
+                    "land_registration_numbers": "Kiine/Kibingoti/Nguguine/2121,  2122,  2124",
+                    "land_location": "district of Kirinyaga"
+                }},
+                {{
+                    "gazette_notice_number": 1015,
+                    "land_holder_names": "Evans Orangi Bogonko, Patrick Ngari Njeru, Aggrey Ogutu",
+                    "land_registration_numbers": "Kilifi/Kikambala Block 285/3/28",
+                    "land_location": "district of Kilifi"
+                }},
+                {{
+                    "gazette_notice_number": 5,
+                    "land_holder_names": "Evans Orangi Bogonko, Patrick Ngari Njeru, Aggrey Ogutu",
+                    "land_registration_numbers": 12345/1, 1456 
+                    "land_location": "district of Kilifi"
+                }}]             
+
     page_text: {page_text}
     """
     classification_prompt = """A page text will be passed to you. If the page text contains mentions of 'THE LAND REGISTRATION ACT'  , the answer should be true else false.
--- src/models/extractor.py
+++ src/models/extractor.py
@@ -3,7 +3,7 @@ from typing import List
 class LandRegistrationAct(BaseModel):
     """fields to be extracted from the gazette"""
     gazette_notice_number: str = Field(..., description="extract the gazette notice number from the information.")
-    land_holder_names: str = Field(..., description="extract comma separated land holder names or name from the information and do not include(ID number) and (deceased) information.")
+    land_holder_names: str = Field(..., description="extract comma separated land holder names or name from the information and do not include(ID number) and (deceased) information or any number associated with the name.")
     land_registration_numbers: str = Field(..., description="Extract land registration number or Known as or leased as. Extract comma separated values if LR and either IR and CR are in the same act.Extract Only the values Don't include the identifiers e.g LR No, IR , CR, Title no , Plot No, Portion No")
     land_location: str = Field(..., description=" Extract one unique location where the land is situated in and You **MUST** include the identifiers e.g district of, city of if mentioned in the act. If none is mentioned return an empty string as the value")
 
